e29657d912b649a2821bcd550c864a9c4f5ad5ed
Senwin.ng <godwinsenwin@outlook.com>
2025-10-17 10:36:16 +0100

    New First index tag

--- ce_cypharm/urls.py
+++ ce_cypharm/urls.py
@@ -3,7 +3,8 @@ from . import views
 #from .views import HomeView, ArticleDetailView, SecondConstructionDetailViewArticleDetailView,VideoImageDetailView
 
 urlpatterns = [
-    #path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
     #path('', HomeView.as_view(), name="home"),
     #path('home/', HomeView.as_view(), name='home'),
     #path('article/<int:pk>/', ArticleDetailView.as_view(), name="detail"),
