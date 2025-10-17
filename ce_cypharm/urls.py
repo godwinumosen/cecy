de5e667ce0b01ad60688ee66cab8a70971238e8d
Senwin.ng <godwinsenwin@outlook.com>
2025-10-17 08:52:14 +0100

    This is for a new project

---
+++ ce_cypharm/urls.py
@@ -0,0 +1,11 @@
+urlpatterns = [
+    #path('index/', views.index, name='index'),
+    #path('', HomeView.as_view(), name="home"),
+    #path('home/', HomeView.as_view(), name='home'),
+    #path('article/<int:pk>/', ArticleDetailView.as_view(), name="detail"),
+    #path('article2/<int:pk>/', SecondConstructionDetailViewArticleDetailView.as_view(), name="second_detail"),
+    #path('about/', AboutView.as_view(), name='about'),
+    #path('contact/', ContactView.as_view(), name='contact'),
+    #path('message/', views.message, name='message'),
+    
+]
