dd53f3e34a8240c6283699f740d73d2ef2b7d00e
Senwin.ng <godwinsenwin@outlook.com>
2025-10-19 13:31:46 +0100

    Sencond index update

--- ce_cypharm/admin.py
+++ ce_cypharm/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 # Register your models here.
 from . import models
-#from .models import DeusMagnusMainPost, SecondDeusMagnusMainPicturePost,SubPicture_2
+from .models import MainImageCarousel
+
+
+
+class MainImageCarouselAdmin (admin.ModelAdmin):
+    prepopulated_fields = {'cecypharm_slug': ('cecypharm_title',)}
+    list_display = ['cecypharm_title','cecypharm_description','cecypharm_publish_date']
+admin.site.register(MainImageCarousel, MainImageCarouselAdmin)
--- ce_cypharm/models.py
+++ ce_cypharm/models.py
@@ -3,3 +3,21 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 from datetime import datetime, date
+
+
+class MainImageCarousel(models.Model):
+    cecypharm_title = models.CharField(max_length=255, blank=True, null=True)
+    cecypharm_description = models.TextField()
+    cecypharm_slug = models.SlugField (max_length=255,blank=True, null=True)
+    cecypharm_image = models.FileField(upload_to='main_image/') 
+    cecypharm_publish_date = models.DateTimeField (auto_now_add= True)
+    cecypharm_author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering =['cecypharm_publish_date']
+    
+    def __str__(self):
+        return self.cecypharm_title + ' | ' + str(self.cecypharm_author)
+    
+    def get_absolute_url(self):
+        return reverse('home',)
