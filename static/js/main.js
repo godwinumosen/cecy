38b89a8096635e7749d39cac0196d4a41807289b
Senwin.ng <godwinsenwin@outlook.com>
2025-11-17 11:23:30 +0100

    Git remove image is been added

--- static/js/main.js
+++ static/js/main.js
@@ -34,7 +34,7 @@
         smartSpeed: 1000,
         dots: false,
         loop: true,
-        nav : true,
+        nav : false,
         navText : [
             '<i class="bi bi-arrow-left"></i>',
             '<i class="bi bi-arrow-right"></i>'
